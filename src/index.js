93f2f8e570028c8a07366c26a7113b0a51c80035
Ramesh Marandi <marandiramesh91@gmail.com>
2024-04-01 23:17:34 +0530

    Util where apiError.js and apiresponse.js created

--- src/index.js
+++ src/index.js
@@ -4,11 +4,23 @@
 // })
 import dotenv from "dotenv"
 import connectDB from './db/index.js';
+import { app } from "./app.js";
 
 dotenv.config({
     path:"./env"
 });
+
+let PORT  = process.env.PORT || 8000 
+
 connectDB()
+.then(()=>{
+    app.listen(PORT, ()=>{
+        console.log(`🎊🎊🎊🎊🎊🎊🎊 Congratulation 🎊🎊🎊🎊🎊🎊🎊: Server is running on PORT : ${PORT}`)
+    })
+
+}).catch(err=>{
+    console.log(`Error connecting to the database ${err}`);
+})
 
 
 /*
